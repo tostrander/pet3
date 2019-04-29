8452a1c9ae6758751473578f78545f24186b372c
Tina Ostrander <tostrander@greenriver.edu>
2019-04-29 12:15:32 -0400

    Add forms

--- index.php
+++ index.php
@@ -8,6 +8,12 @@ require_once('vendor/autoload.php');
 //Create an instance of the Base class
 $f3 = Base::instance();
 
+//Define an array of valid colors
+$f3->set('colors', array('pink', 'green', 'blue'));
+//$f3->set('errors', array());
+
+require_once('model/validation-functions.php');
+
 $f3->route('GET /',
     function() {
         echo "<link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css'>";
@@ -15,30 +21,45 @@ $f3->route('GET /',
         echo "<a href='./order'>Order a pet</a>";
     });
 
-$f3->route('GET /order',
-    function() {
-        $view = new View;
-        echo $view->render('views/form1.html');
-    });
+$f3->route('GET|POST /order',
 
-$f3->route('POST /form2',
-    function() {
-
-        $_SESSION['animal'] = $_POST['animal'];
-        //echo "animal: ".$_SESSION['animal'];
-
-        $view = new View;
-        echo $view->render('views/form2.html');
-    });
-
-$f3->route('POST /results/@type',
     function($f3) {
 
-        $_SESSION['color'] = $_POST['color'];
+        $_SESSION = array();
 
-        //Create global variables
-        $f3->set('animal', $_SESSION['animal']);
-        $f3->set('color', $_SESSION['color']);
+        if (isset($_POST['next'])) {
+            $animal = $_POST['animal'];
+            if (validText($animal)) {
+                $_SESSION['animal'] = $animal;
+                $f3->reroute('/order2');
+            } else {
+                $f3->set("errors['animal']", "Please enter an animal.");
+            }
+        }
+
+        $template = new Template();
+        echo $template->render('views/form1.html');
+    });
+
+$f3->route('GET|POST /order2',
+    function($f3) {
+
+        if (isset($_POST['submit'])) {
+            $color = $_POST['color'];
+            if (validColor($color)) {
+                $_SESSION['color'] = $color;
+                $f3->reroute('/results');
+            } else {
+                $f3->set("errors['color']", "Please select a color.");
+            }
+        }
+
+        $template = new Template();
+        echo $template->render('views/form2.html');
+    });
+
+$f3->route('GET /results',
+    function($f3) {
 
         //load a page using a Template
         $template = new Template();
